d08bd7f5aed4dec434fb1d889acdf1e57f5cfb75
Achilcraft <39186535+achilj@users.noreply.github.com>
2023-11-28 18:37:57 +0100

    Revert "Small fix"
    This reverts commit 7cf5f9b6dc7a2ef0da08af57cfa1075eb1c598a1.

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
+// Gebruik browser.runtime.onInstalled in plaats van browser.runtime.onInstalled
 browser.runtime.onInstalled.addListener(function () {
+    // Gebruik browser.declarativeContent.onPageChanged in plaats van browser.declarativeContent.onPageChanged
     browser.declarativeContent.onPageChanged.removeRules(undefined, function () {
         browser.declarativeContent.onPageChanged.addRules([{
             conditions: [
+                // Gebruik browser.declarativeContent.PageStateMatcher in plaats van browser.declarativeContent.PageStateMatcher
                 new browser.declarativeContent.PageStateMatcher({
                     pageUrl: { hostContains: '.smartschool.be' },
                 })
