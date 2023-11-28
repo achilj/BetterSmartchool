ed5296d201eca95c244551ac59178f6de1586a87
Achilcraft <39186535+achilj@users.noreply.github.com>
2023-11-28 19:18:27 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,7 @@
-// Gebruik browser.runtime.onInstalled in plaats van browser.runtime.onInstalled
 browser.runtime.onInstalled.addListener(function () {
-    // Gebruik browser.declarativeContent.onPageChanged in plaats van browser.declarativeContent.onPageChanged
     browser.declarativeContent.onPageChanged.removeRules(undefined, function () {
         browser.declarativeContent.onPageChanged.addRules([{
             conditions: [
-                // Gebruik browser.declarativeContent.PageStateMatcher in plaats van browser.declarativeContent.PageStateMatcher
                 new browser.declarativeContent.PageStateMatcher({
                     pageUrl: { hostContains: '.smartschool.be' },
                 })
